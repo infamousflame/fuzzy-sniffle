e2288875193d4c98eb7ac7cdcdc446fd460a01bd
infamousflame <67448147+infamousflame@users.noreply.github.com>
2024-07-17 10:10:49 +1200

    Create main.py
    Created the main file

---
+++ src/main.py
@@ -0,0 +1,8 @@
+"""A simple program that allows the user to encrypt and decrypt thier private
+files.
+"""
+
+from modules.ui.create_ui import MainUI
+
+if __name__ == '__main__':
+    MainUI().run()
